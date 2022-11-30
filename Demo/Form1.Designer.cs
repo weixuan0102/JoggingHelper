64723cd05da3c061044fe46ee7df4f50fba5d7e3
illdoCc <103tony50505@gmail.com>
2022-11-30 20:27:50 +0800

    jhgkhg

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.weatherInfo = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.新增紀錄ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
@@ -97,25 +100,55 @@
             this.新增紀錄ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1159, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1159, 25);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // 新增紀錄ToolStripMenuItem
             // 
             this.新增紀錄ToolStripMenuItem.Name = "新增紀錄ToolStripMenuItem";
-            this.新增紀錄ToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.新增紀錄ToolStripMenuItem.Size = new System.Drawing.Size(72, 21);
             this.新增紀錄ToolStripMenuItem.Text = "新增紀錄";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "慢跑紀錄";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "天氣";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "飲食紀錄";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1159, 566);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "健康管理";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
@@ -136,6 +169,9 @@
         private System.Windows.Forms.Label weatherInfo;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 新增紀錄ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
