cba2a52e6f4ffaaa386a982fc73a60ee6a07a036
William Lee <leewe2014@gmail.com>
2022-11-30 20:09:26 +0800

    Get weather information successfully.

--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -17,11 +17,15 @@ namespace Demo
 {
     public partial class Form1 : Form
     {
-        const string api_key = "78027a78-cebc-4832-8025-ac732908f298";
-        const string api_url_prefix = "https://data.epa.gov.tw/api/v2";
-        string[] api_url_postfix = { "/aqx_p_432" };
+        const string air_api_key = "78027a78-cebc-4832-8025-ac732908f298";
+        const string weather_api_key = "CWB-131BB899-463A-4D5B-B30E-7FD3F0040531";
+        const string air_api_url_prefix = "https://data.epa.gov.tw/api/v2";
+        const string weather_api_url_prefix = "https://opendata.cwb.gov.tw/api";
+        const string postfix_air = "/aqx_p_432";
+        const string postfix_weather = "/v1/rest/datastore/F-C0032-001";
+        const string location = "%E8%87%BA%E5%8D%97%E5%B8%82"; // Tainan
         const string offset = "0", limit = "100", format = "JSON";
-        string api_url_setting = $"?offset={offset}&limit={limit}&api_key={api_key}&format={format}";
+        string api_url_setting = $"?offset={offset}&limit={limit}&api_key={air_api_key}&format={format}";
         public Form1()
         {
             InitializeComponent();
@@ -30,60 +34,96 @@ namespace Demo
         private void Form1_Load(object sender, EventArgs e)
         {
             weatherInfo.Text = "NULL";
+            weatherInfo.Font = new Font(weatherInfo.Font.FontFamily,16);
         }
 
         private void getWeatherBtn_Click(object sender, EventArgs e)
         {
-            var w = Task<Weather>.Run(async () => await GetRequestApi());
-            
-            weatherInfo.Text = w.Result.GetWeatherInfo();
+            var w = Task<Output>.Run(async () => await GetRequestApi());
+            weatherInfo.Text = w.Result.GetInfo();
         }
-        private async Task<Weather> GetRequestApi()
+        private async Task<Output> GetRequestApi()
         {
-            Weather w = new Weather();
-            for (int i = 0; i < api_url_postfix.Length; i++)
-            {
-                string URL = api_url_prefix + api_url_postfix[i] + api_url_setting;
-                try
-                {
-                    using (HttpClient httpClient = new HttpClient())
-                    {
-                        HttpResponseMessage httpResponse = await httpClient.GetAsync(URL);
-                        httpResponse.EnsureSuccessStatusCode();
-                        var requestBody = await httpResponse.Content.ReadAsStringAsync();
-                        dynamic json = JsonSerializer.Deserialize<dynamic>(requestBody);
-                        //台南市 台南的空氣測站
-                        string aqi = json.GetProperty("records")[45].GetProperty("aqi").ToString();
-                        string pollutant = json.GetProperty("records")[45].GetProperty("pollutant").ToString();
-                        string status = json.GetProperty("records")[45].GetProperty("status").ToString();
-                        string info = $"AQI值:{aqi}\r\n汙染物:{pollutant}\r\n狀況:{status}";
-                        w.ConcatenateInfo(info);
-                    }
-                }
-                catch (HttpRequestException e)
-                {
-                    MessageBox.Show($"Message :{e.Message} ", "Exception Caught!");
-                }
+            Output w = new Output();
 
+            string URL = air_api_url_prefix + postfix_air + api_url_setting;
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponse = await httpClient.GetAsync(URL);
+                    httpResponse.EnsureSuccessStatusCode();
+                    var requestBody = await httpResponse.Content.ReadAsStringAsync();
+                    dynamic json = JsonSerializer.Deserialize<dynamic>(requestBody);
+                    //台南市 台南的空氣測站
+                    string sitename = json.GetProperty("records")[45].GetProperty("sitename").ToString();
+                    w.SetInfo(0, sitename);
+                    string wind_speed = json.GetProperty("records")[45].GetProperty("wind_speed").ToString() + " m/sec";
+                    w.SetInfo(1, wind_speed);
+                    string aqi = json.GetProperty("records")[45].GetProperty("aqi").ToString();
+                    w.SetInfo(2, aqi);
+                    string pollutant = json.GetProperty("records")[45].GetProperty("pollutant").ToString();
+                    w.SetInfo(3, pollutant);
+                    string status = json.GetProperty("records")[45].GetProperty("status").ToString();
+                    w.SetInfo(4, status);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                MessageBox.Show($"Message :{e.Message} ", "Exception Caught!");
+            }
+
+            URL = weather_api_url_prefix + postfix_weather + $"?Authorization={weather_api_key}&format={format}&locationName={location}";
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponse = await httpClient.GetAsync(URL);
+                    httpResponse.EnsureSuccessStatusCode();
+                    var requestBody = await httpResponse.Content.ReadAsStringAsync();
+                    dynamic json = JsonSerializer.Deserialize<dynamic>(requestBody);
+                    //台南市 台南的天氣測站
+                    var startTime = json.GetProperty("records").GetProperty("location")[0].GetProperty("weatherElement")[4].GetProperty("time")[0].GetProperty("startTime").ToString();
+                    var endTime = json.GetProperty("records").GetProperty("location")[0].GetProperty("weatherElement")[4].GetProperty("time")[0].GetProperty("endTime").ToString();
+                    var maxTemperature = json.GetProperty("records").GetProperty("location")[0].GetProperty("weatherElement")[4].GetProperty("time")[0].GetProperty("parameter").GetProperty("parameterName").ToString();
+                    var minTemperature = json.GetProperty("records").GetProperty("location")[0].GetProperty("weatherElement")[2].GetProperty("time")[0].GetProperty("parameter").GetProperty("parameterName").ToString();
+                    var PoP = json.GetProperty("records").GetProperty("location")[0].GetProperty("weatherElement")[1].GetProperty("time")[0].GetProperty("parameter").GetProperty("parameterName").ToString();
+                    w.SetInfo(5, maxTemperature + "C");
+                    w.SetInfo(6, minTemperature + "C");
+                    w.SetInfo(7, PoP + " %\r\n" + startTime + "~" + endTime);
+
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                MessageBox.Show($"Message :{e.Message} ", "Exception Caught!");
             }
             return w;
-
         }
     }
-    public class Weather
+    public class Output
     {
-        private string weather_info = "";
-        public Weather()
+        //string sitename, windSpeed, aqi, pollutant, status, temperature
+        private string[] weather_info;
+        private string[] prefix = { "測站名稱: ", "風速: ", "AQI值: ", "汙染物: ", "空氣狀況: ", "最高溫度: ", "最低溫度: ", "降雨機率: " };
+        public Output()
         {
+            weather_info = new string[prefix.Length];
+        }
+        public void SetInfo(int idx, string input)
+        {
+            if (idx < 0 || idx >= weather_info.Length) return;
 
+            weather_info[idx] = input;
         }
-        public void ConcatenateInfo(string input)
+        public string GetInfo()
         {
-            weather_info += input + "\r\n";
-        }
-        public string GetWeatherInfo()
-        {
-            return weather_info;
+            string output = "";
+            for (int i = 0; i < weather_info.Length; i++)
+            {
+                output += prefix[i] + weather_info[i] + "\r\n";
+            }
+            return output;
         }
     }
 }
